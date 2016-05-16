f6b5c62c7b3a923ed185ef82762192e55b37fe49
Preben Asmussen <preben.asmussen@gmail.com>
2016-05-16 16:23:29 +0200

    introduce parent bom

--- play-service/src/main/java/dk/test/Application.java
+++ play-service/src/main/java/dk/test/Application.java
@@ -1,22 +1,10 @@
 package dk.test;
 
-import com.codahale.metrics.MetricRegistry;
-
-import org.apache.camel.RoutesBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    private RoutesBuilder[] routesBuilders;
-
-    @Autowired
-    private MetricRegistry metricRegistry;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
