dea2969a94d36ebb22c8e98b124632aa42062982
Preben Asmussen <preben.asmussen@gmail.com>
2016-10-29 20:37:39 +0200

    lets use fabric8 default port

--- play-client/src/main/java/dk/test/play/ClientApplication.java
+++ play-client/src/main/java/dk/test/play/ClientApplication.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,8 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @SpringBootApplication
 @EnableJms
 public class ClientApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ClientApplication.class);
 
     private static List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
@@ -46,14 +50,17 @@ public class ClientApplication {
 
         @Override
         public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+            log.info("Closing sesseion {}", session);
             sessions.remove(sessions);
         }
     }
 
     @JmsListener(destination = "playlist")
-    public void handleKafkaMessage(String payload) throws IOException {
+    public void handleMessage(String payload) throws IOException {
         for (WebSocketSession session : sessions) {
-            session.sendMessage(new TextMessage(payload));
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(payload));
+            }
         }
     }
 
